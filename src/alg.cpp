8396070c633149b3bbb34e764c579bc22b7c231a
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-02-27 15:06:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -35,15 +35,15 @@ return (pown(x, n) / fact(n));
 
 double expn(double x, uint16_t count) {
 double value = 0;
-for (uint64_t i = 0; i < count; i++) {
-  value = value + calcItem(x, i);
+while (count > 0) {
+  value = value + calcItem(x, count);
 }
   return value;
 }
 
 double sinn(double x, uint16_t count) {
 double value = 0;
-for (uint64_t i = 1; i < count; i++) {
+for (uint64_t i = 1; i <= count; i++) {
   value = value + pown(-1, i - 1) * pown(x, 2 * i - 1) / fact(2 * i - 1);
 }
   return value;
@@ -51,7 +51,7 @@ for (uint64_t i = 1; i < count; i++) {
 
 double cosn(double x, uint16_t count) {
 double value = 0;
-for (uint64_t i = 1; i < count; i++) {
+for (uint64_t i = 1; i <= count; i++) {
   value = value + pown(-1, i - 1) * pown(x, 2 * i - 2) / fact(2 * i - 2);
 }
   return value;
